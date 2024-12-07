c146e07ff425fb860e1b4afd459176fa858bd6bf
Kirill Efimenko <vivichv9@yandex.ru>
2024-12-07 17:29:11 +0300

    fix pre commit

--- ml/prediction.py
+++ ml/prediction.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from catboost import CatBoostClassifier, Pool
-from typing import Dict, Tuple, Any
 from sklearn.model_selection import train_test_split
 
 RANDOM_STATE = 42
 
-class BrokeClassification():
+
+class BrokeClassification:
     def __init__(self, data: pd.DataFrame, target: pd.DataFrame):
         """
         Args:
@@ -13,7 +13,9 @@ class BrokeClassification():
             target (pd.DataFrame): predicted_value for train model
         """
 
-        X_train, y_train, X_test, y_test = train_test_split(data, target, test_size=0.3, random_state=RANDOM_STATE)
+        X_train, y_train, X_test, y_test = train_test_split(
+            data, target, test_size=0.3, random_state=RANDOM_STATE
+        )
 
         self.data = X_train
         self.target = y_train
@@ -21,21 +23,25 @@ class BrokeClassification():
         self.target_test = y_test
         self.model = None
 
-        self.categorical_features = self.data.select_dtypes(include=["object", "category"]).columns.tolist() 
+        self.categorical_features = self.data.select_dtypes(
+            include=["object", "category"]
+        ).columns.tolist()
 
     def param_selection(self):
         """
         Selection model parameters with Grid search
-        
+
         Returns:
             Dictionary with best model parameters
         """
 
-        train_data = Pool(data=self.data, label=self.target, cat_features=self.categorical_features)
+        train_data = Pool(
+            data=self.data, label=self.target, cat_features=self.categorical_features
+        )
         print(train_data.get_features_name())
 
         param_grid = {
-            "iterations": [100, 200, 300, 400], 
+            "iterations": [100, 200, 300, 400],
             "learning_rate": [0.01, 0.05, 0.1],
             "depth": [1, 2, 3],
             "l2_leaf_reg": [1, 3, 5],
@@ -53,15 +59,13 @@ class BrokeClassification():
         self.model = model
         self.model.set_params(**grid_search_result["params"])
 
-
     def train(self):
         self.param_selection()
         self.model.fit(self.data, self.target, cat_features=self.categorical_features)
 
         test_score = self.model.score(self.data_test, self.target_test)
         print(f"Test score: {test_score}")
-    
 
     def prediction(self, data):
-        prediction = self.model.predict(data=data, prediction_type='Class')
+        prediction = self.model.predict(data=data, prediction_type="Class")
         return prediction
